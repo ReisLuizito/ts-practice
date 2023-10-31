d8eea15c58092cf54506219c619b398424b5712b
Luiz Guilherme Reis <luiz.guilherme.dev@outlook.com>
2023-10-31 12:33:38 -0300

    Typescript types examples

---
+++ script.js
@@ -0,0 +1,17 @@
+const frase = 'Front End';
+const total = 100.05;
+const empresas = ['Apple', 'Microsoft'];
+const body = document.body;
+const operacao = 100 + true;
+
+console.log(frase.toLowerCase());
+
+console.log (total.toFixed())
+
+console. log (empresas.map((empresa) => empresa.toLowerCase()));
+
+body.style.background = "#000"
+
+console.log(operacao)
+
+
